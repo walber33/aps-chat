4485d02e2c74946667b6f577dd0eba0a47cc33aa
CCJorge <jorgesouza1996@outlook.com>
2018-05-01 15:59:20 -0300

    mudancas pra ajeitar o login de cliente

--- src/chatambiental/ClienteThread.java
+++ src/chatambiental/ClienteThread.java
@@ -9,9 +9,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import static chatambiental.ServidorThread.msgParaTodos;
+import dao.LoginDAO;
 import view.ServerView;
 import chatambiental.ServidorThread;
-import view.ServerView;
 
 /**
  *
@@ -20,19 +21,59 @@ import view.ServerView;
 public class ClienteThread implements Runnable {
 
     public String usuario, mensagem;
+    private String senha;
     public BufferedReader in = null;
     public PrintStream out = null;
     public Socket sck = null;
+    private boolean logado = false;
 
-    public ClienteThread(String usuario, Socket sck, BufferedReader in, PrintStream out) {
-        this.usuario = usuario;
+    public ClienteThread(Socket sck, BufferedReader in, PrintStream out) {
         this.sck = sck;
         this.in = in;
         this.out = out;
-        ServidorThread.users.add(this);
+    }
+
+    private void fecharCliente() {
+        try {
+            this.in.close();
+            this.out.close();
+            this.sck.close();
+        } catch (IOException ex) {
+            System.out.println("Não consgeuiu fechar a conexão\n");
+            ex.printStackTrace();
+        }
     }
 
     public void run() {
+        while (!logado) {
+            try {
+                usuario = in.readLine();
+                senha = in.readLine();
+                if (LoginDAO.verificarCredenciais(usuario, senha)) {
+                    if (!ServidorThread.estaLogado(usuario)) {
+                        ServerView.log("Usuario " + usuario + " conectado\n");
+                        out.println("true");
+                        out.println("Conectado ao servidor");
+                        msgParaTodos("Usuario " + usuario + " se conectou", this);
+                        ServidorThread.users.add(this);
+                        logado = true;
+                    } else {
+                        ServerView.log("Tentiva de login recusado: usuário já está logado\n");
+                        out.println("false");
+                        out.println("Usuario já está logado");
+                    }
+                } else {
+                    ServerView.log("Tentiva de login recusado: usuário ou senha inválidos\n");
+                    out.println("false");
+                    out.println("Usuario ou senha inválidos");
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                fecharCliente();
+                return;
+            }
+        }
+
         try {
             while ((mensagem = this.in.readLine()) != null) {
                 ServidorThread.msgParaTodos(mensagem, this);
@@ -41,14 +82,7 @@ public class ClienteThread implements Runnable {
         } catch (IOException ex) {
             System.out.println("Não conseguiu ler do Socket do Cliente " + this.usuario);
         } finally {
-            try {
-                this.in.close();
-                this.out.close();
-                this.sck.close();
-            } catch (IOException ex) {
-                System.out.println("Não consgeuiu fechar a conexão\n");
-            }
-
+            fecharCliente();
             ServidorThread.users.remove(this);
             ServerView.log("Usuario " + this.usuario + " desconectado\n");
             ServidorThread.msgParaTodos("Usuario " + usuario + " desconectou\n");
